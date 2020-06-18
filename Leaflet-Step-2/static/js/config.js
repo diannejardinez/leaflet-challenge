402e561d2f6880ec3627ba7d5174a56a65298f93
diannejardinez <45705176+diannejardinez@users.noreply.github.com>
2020-06-18 02:10:20 -0700

    added public API key for GitHub.io

--- Leaflet-Step-2/static/js/config.js
+++ Leaflet-Step-2/static/js/config.js
@@ -1,3 +1,3 @@
 // API key
-// Limted Access for Public GitHub Repo
+// Limited Access for Public GitHub Repo
 const API_KEY = "pk.eyJ1IjoiZGphcmRpbmV6IiwiYSI6ImNrYmtrNWFmcTEwMDAycHFuMmdmbzN2Y2EifQ.RpL3mC5WB6dwt2xTu1s7Hg";
